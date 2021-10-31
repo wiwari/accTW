9480d671430238c2468057b7bf5824870b572b6f
wiwari <wiwari@protonmail.com>
2021-10-31 16:00:01 +0800

    test cross origin proxy

--- accTW-ll.js
+++ accTW-ll.js
@@ -772,6 +772,7 @@ lyctrl.addOverlay(waterlevelLayer, "水利署水位站");
     // Query Station of Water Level  //https://data.wra.gov.tw/Service/OpenData.aspx?format=json&id=28E06316-FE39-40E2-8C35-7BF070FD8697
     
     // await $.getJSON("http://localhost:8080/28E06316-FE39-40E2-8C35-7BF070FD8697.json")
+    // await $.getJSON("http://localhost:8070/https://data.wra.gov.tw/Service/OpenData.aspx?format=json&id=28E06316-FE39-40E2-8C35-7BF070FD8697")
     await $.getJSON("https://api.allorigins.win/get?url=https%3A//data.wra.gov.tw/Service/OpenData.aspx%3Fformat%3Djson%26id%3D28E06316-FE39-40E2-8C35-7BF070FD8697&callback=?")
       .done(function (data) {
         // console.log("second success");
