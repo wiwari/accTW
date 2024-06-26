eb9deb39c416518c084c9fd072daa34821a026d8
wiwari <wiwari@protonmail.com>
2024-06-26 12:37:28 +0800

    add url-gpx drag and drop support

--- dropLocalItem.js
+++ dropLocalItem.js
@@ -1,4 +1,5 @@
 // Enable local GPX layer loading by drag and drop on map
+// Enable online GPX layer loading by drag and drop on map
 
 function dropHandler(ev) {
     // Prevent default behavior (Prevent file from being opened)
@@ -7,29 +8,32 @@ function dropHandler(ev) {
     if (!ev.ctrlKey) {  //append gpx layers while holding ctrl
         local_gpxlayers.clearLayers();
     }
-
-    // console.log('File(s) dropped');
+    
     if (ev.dataTransfer.dropEffect == "copy" || true) {
-        if (ev.dataTransfer.items) {
-           
+        if(ev.dataTransfer.files){
             // Use DataTransferItemList interface to access the file(s)
             [...ev.dataTransfer.items].forEach((item, i) => {
                 // If dropped items aren't files, reject them
                 if (item.kind === 'file') {
                     const file = item.getAsFile();
                     // console.log(`… file[${i}].name = ${file.name} ` + file.lastModified);
-
                     loadMyLocalGPX(file);
-                }
-            });
-
-
-        } else {
-            // Use DataTransfer interface to access the file(s)
-            [...ev.dataTransfer.files].forEach((file, i) => {
-                console.log(`… file[${i}].name = ${file.name}  `);
+                } 
             });
         }
+
+        if (ev.dataTransfer.items) {
+            // Load from dropped gpx URL
+            if (url = ev.dataTransfer.getData('text/uri-list') ){
+                loadOnlineGPX(url);
+            }else if (url = ev.dataTransfer.getData('text/x-moz-url')){
+                loadOnlineGPX(url);
+            }else if (url = ev.dataTransfer.getData('public.url')){
+                loadOnlineGPX(url);
+            }else if (url = ev.dataTransfer.getData('text/plain')){
+                loadOnlineGPX(url);
+            }
+        } 
     }    
 
 }
@@ -45,14 +49,14 @@ function dragOverHandler(ev) {
         ev.dataTransfer.dropEffect = 'none';
     } else if (ev.altKey) {
         // ev.dataTransfer.dropEffect = 'link';     
-        ev.dataTransfer.dropEffect = 'none';   
+        ev.dataTransfer.dropEffect = 'none';
     } else {
         ev.dataTransfer.dropEffect = 'copy';
     }
 
     // ev.dataTransfer.dropEffect = 'copy';
     [...ev.dataTransfer.items].forEach((item, i) => {
-        if (!(item.kind === 'file')) {
+        if (!(item.kind === 'file' || item.kind === 'string')) {
             // console.log(`not support other kind of input other than file: ${item.kind}`);  //eg: string
             ev.dataTransfer.dropEffect = 'none';
         }
@@ -61,50 +65,64 @@ function dragOverHandler(ev) {
     // console.log('File(s) in drop zone');
 }
 
-function loadMyLocalGPX(file){ //load from INPUT file or drop
+function loadGPX(url){
+    lygpx = new L.GPX(url, {
+        async: true,
+        gpx_options: {
+            parseElements: ['track'], // ['track', 'route', 'waypoint']
+        },
+        marker_options: {
+            startIconUrl: 'https://github.com/mpetazzoni/leaflet-gpx/raw/main/pin-icon-start.png',
+            endIconUrl: 'https://github.com/mpetazzoni/leaflet-gpx/raw/main/pin-icon-end.png',
+            shadowUrl: 'https://github.com/mpetazzoni/leaflet-gpx/raw/main/pin-shadow.png',
+            wptIconUrls: {
+                '': 'https://github.com/mpetazzoni/leaflet-gpx/raw/main/pin-icon-wpt.png',
+            },
+
+        },
+        polyline_options: {
+            color: 'red',
+            opacity: 0.5,
+            weight: 5,
+            lineCap: 'round'
+        }
+    }).on('loaded', function (e) {
+        local_gpxlayers.addLayer(lygpx);
+        // lyctrl.addOverlay(e.target, '<span class="btn-sm"><i class="fa fa-arrow-circle-right text-black-50" aria-hidden="true"></i></span>' + e.target.get_name());
+        map.fitBounds(local_gpxlayers.getBounds());
+    }).addTo(map);
+}
+
+function loadOnlineGPX(url) { //load from INPUT file or drop
     const regxFilename = /^(.*)\.[gG][pP][xX]$/;
-                    if (file.name.length > 0) //URL papameter format: ?center=lat,lng
-                        if (found = file.name.match(regxFilename)) {
-                            // console.log(`GPX supported: ${file.name} , Modified ${file.lastModified}`);
+    if (typeof (url) === 'string' && url.length > 0) //URL papameter format: ?center=lat,lng
+        if (found = url.match(regxFilename)) {
+            loadGPX(url);
+        } else {
+            console.log(`file type not supported: ${url}`); //not GPX file
+            ;
+        }
 
-                            const reader = new FileReader();
-                            reader.addEventListener('load', (event) => {
-                                // console.log(event.target.result);
-                                var gpx = event.target.result; // URL to your GPX file or the GPX itself
-                                lygpx = new L.GPX(gpx, {
-                                    async: true,
-                                    gpx_options: {
-                                        parseElements: ['track'], // ['track', 'route', 'waypoint']
-                                    },
-                                    marker_options: {
-                                        startIconUrl: 'https://github.com/mpetazzoni/leaflet-gpx/raw/main/pin-icon-start.png',
-                                        endIconUrl: 'https://github.com/mpetazzoni/leaflet-gpx/raw/main/pin-icon-end.png',
-                                        shadowUrl: 'https://github.com/mpetazzoni/leaflet-gpx/raw/main/pin-shadow.png',
-                                        wptIconUrls: {
-                                            '': 'https://github.com/mpetazzoni/leaflet-gpx/raw/main/pin-icon-wpt.png',
-                                        },
-
-                                    },
-                                    polyline_options: {
-                                        color: 'red',
-                                        opacity: 0.5,
-                                        weight: 5,
-                                        lineCap: 'round'
-                                    }
-                                }).on('loaded', function (e) {
-                                    local_gpxlayers.addLayer(lygpx);
-                                    // lyctrl.addOverlay(e.target, '<span class="btn-sm"><i class="fa fa-arrow-circle-right text-black-50" aria-hidden="true"></i></span>' + e.target.get_name());
-                                    map.fitBounds(local_gpxlayers.getBounds());
-                                }).addTo(map);
-                                
-                            });
-                            reader.readAsDataURL(file);
+}
 
 
-                        } else {
-                            // console.log(`file type not supported: ${file.name}`); //not GPX file
-                            ;
-                        }
+function loadMyLocalGPX(file) { //load from INPUT file or drop
+    const regxFilename = /^(.*)\.[gG][pP][xX]$/;
+    if (file.name.length > 0) //URL papameter format: ?center=lat,lng
+        if (found = file.name.match(regxFilename)) {
+            // console.log(`GPX supported: ${file.name} , Modified ${file.lastModified}`);
+
+            const reader = new FileReader();
+            reader.addEventListener('load', (event) => {
+                // console.log(event.target.result);
+                gpx = event.target.result; // URL to your GPX file or the GPX itself
+                loadGPX(gpx);
+            });
+            reader.readAsDataURL(file);
+        } else {
+            console.log(`file type not supported: ${file.name}`); //not GPX file
+            ;
+        }
 
 }
 
