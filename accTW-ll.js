fbe8da76f8470255fb9c6884ef0e047aa9ea99d9
wiwari <wiwari@protonmail.com>
2024-07-29 20:34:53 +0800

    use iframe popup in reservoir

--- accTW-ll.js
+++ accTW-ll.js
@@ -1319,18 +1319,43 @@ const wraRES = L.geoJSON([], {
   // API filter example
   // "https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Station?$filter=StationName eq '羅好壩'"
   // "https://fhy.wra.gov.tw/WraApi/v1/Reservoir/Daily?$filter=StationNo eq '10213'&$select=InflowTotal,OutflowTotal,Time"
-  popupinfo = "";
-  popupinfo += '<div class="container-sm">';
-  popupinfo += layer.feature.properties.name ;
-  popupinfo += (layer.feature.properties.id) ? "(" + layer.feature.properties.id + ")" :'';
-  popupinfo += "<br/>";  
-  popupinfo += (layer.feature.properties.date) ? layer.feature.properties.date.replace(/(....-..-..)T.*/, "$1") + "<br/>" : '';
-  popupinfo += (layer.feature.properties.InflowTotal) ? "流入" + (layer.feature.properties.InflowTotal * 10000 / 24 / 60 / 60).toFixed(2) + "<sub>m³/s</sub><br/>" : '';
-  popupinfo += (layer.feature.properties.OutflowTotal) ? "流出" + (layer.feature.properties.OutflowTotal * 10000 / 24 / 60 / 60).toFixed(2) + "<sub>m³/s</sub><br/>" : '';
-  popupinfo += (wrafhy_activeStationNo.includes(parseInt(layer.feature.properties.id)))? '<a href="https://wiwari.github.io/wra-fhy/?StationNo='+ layer.feature.properties.id +' " target="_blank" class="btn btn-outline-primary btn-sm">近日平均流量</a>' : '';
-  popupinfo += '</div>';
 
-  return popupinfo;
+  const popupStationContainer = L.DomUtil.create('div',"container-sm");
+  popupStationContainer.appendChild(document.createTextNode(layer.feature.properties.name));
+  if (layer.feature.properties.id ) 
+    {
+    popupStationContainer.appendChild(document.createTextNode(`(${layer.feature.properties.id})`));
+    L.DomUtil.create('br','',popupStationContainer);
+    }    
+  if (layer.feature.properties.date){
+    popupStationContainer.appendChild(document.createTextNode(layer.feature.properties.date.replace(/(....-..-..)T.*/, "$1")));
+    L.DomUtil.create('br','',popupStationContainer);
+
+  }
+  if (layer.feature.properties.InflowTotal){
+    popupStationContainer.appendChild(document.createTextNode("流入" + (layer.feature.properties.InflowTotal * 10000 / 24 / 60 / 60).toFixed(2)));
+    const subElement=L.DomUtil.create('sub','',popupStationContainer);
+    subElement.appendChild(document.createTextNode("m³/s"));
+    L.DomUtil.create('br','',popupStationContainer);
+  }
+  if (layer.feature.properties.OutflowTotal){
+    popupStationContainer.appendChild(document.createTextNode("流出" + (layer.feature.properties.OutflowTotal * 10000 / 24 / 60 / 60).toFixed(2)));
+    const subElement=L.DomUtil.create('sub','',popupStationContainer);
+    subElement.appendChild(document.createTextNode("m³/s"));
+    L.DomUtil.create('br','',popupStationContainer);
+  }
+  if ((wrafhy_activeStationNo.includes(parseInt(layer.feature.properties.id)))){
+    const linkElement=L.DomUtil.create('a','btn btn-outline-primary btn-sm',popupStationContainer);
+    linkElement.text='近日平均流量';
+    linkElement.href=`https://wiwari.github.io/wra-fhy/?StationNo=${layer.feature.properties.id}`;
+    linkElement.target='_blank';
+    L.DomEvent.disableClickPropagation(linkElement);
+    L.DomEvent.on(linkElement, 'click', function(e) {  
+      e.preventDefault();
+      openDialog(linkElement.href);
+    });
+  }
+  return popupStationContainer;
      
 });
 
